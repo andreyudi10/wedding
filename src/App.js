a3eaaa293340e3ce132bddc70667ccda4b995fd7
andreyudi10 <andrianus.yudi@gmail.com>
2020-12-04 21:58:23 +0700

    andri

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { MDBBtn } from "mdbreact";
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-      return
-    </div>
+    <Fragment>
+      <div>
+      halo
+      </div>
 
+      <MDBBtn color="primary">Primary</MDBBtn>
+      <MDBBtn>Default</MDBBtn>
+      <MDBBtn color="secondary">Secondary</MDBBtn>
+      <MDBBtn color="success">Success</MDBBtn>
+      <MDBBtn color="info">Info</MDBBtn>
+      <MDBBtn color="warning">Warning</MDBBtn>
+      <MDBBtn color="danger">Danger</MDBBtn>
+    </Fragment>
   );
 }
 
